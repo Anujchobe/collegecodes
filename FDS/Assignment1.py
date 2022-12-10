50f47100eada5ca56ec34af84228d22a88bfc3df
Anujchobe <98049418+Anujchobe@users.noreply.github.com>
2022-12-10 11:56:38 +0530

    errors solves
    Still functions not used

--- FDS/Assignment1.py
+++ FDS/Assignment1.py
@@ -30,8 +30,12 @@ print(f"Average score of class = {sum/ctr}")
 ab=0
 min=0
 max=0
-for i in mrklist:
+ct=0
+for i in mrklist:        
     if i!="ab":
+        if ct==0 :
+            min=i
+            ct+=1
         if min>i:
             min=i
         if max<i:
@@ -39,19 +43,24 @@ for i in mrklist:
     else:
         ab+=1
 print(f"Highest score of class = {max} and Lowest score of class= {min}")        
-print(f"Number of students that we absent = {ab}")
+print(f"Number of students that were absent = {ab}")
 
 #Frequency
 freq={}
 mfreq=0
 for i in mrklist:
     if i !="ab":
-        if freq.get(i)==None:
+        if i!=freq.keys():
             freq[i]=1
         else:
             freq[i]+=1
-        if mfreq<freq[i]:
-            mfreq=freq[i]
-print(f"Marks with highest Frequency = {freq.get(mfreq)}")
+
+for i in freq:
+    if mfreq<freq[i]:
+            mfreq=freq[i]        
+key=list(freq.keys()) 
+val=list(freq.values())                     
+pos=val.index(mfreq)
+print("Marks with highest Frequency = ",key[pos])
 
 
